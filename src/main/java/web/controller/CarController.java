6cf9948b4cdc9adf3c52f2285f6bda76ce73b6d6
Hrust1981 <rustamshar@inbox.ru>
2023-02-19 11:49:31 +0500

    Java pre-project. Задача 2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,20 +3,20 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImp;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
     private List<Car> cars;
+
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(value = "count", required = false) int count,
-                            ModelMap model) {
+    public String printCars(HttpServletRequest servletRequest, ModelMap model) {
         cars = new ArrayList<>();
         cars.add(new Car("Mercedes-Benz", "C300", 250));
         cars.add(new Car("BMW", "M3", 350));
@@ -24,9 +24,11 @@ public class CarController {
         cars.add(new Car("Huyndai", "Getz", 97));
         cars.add(new Car("KIA", "Ceed", 150));
 
+        int param = servletRequest.getParameter("count") == null
+                ? cars.size()
+                : Integer.parseInt(servletRequest.getParameter("count"));
         CarService carService = new CarServiceImp(cars);
-        List<Car> numberOfCars = carService.getNumberOfCars(count);
-
+        List<Car> numberOfCars = carService.getNumberOfCars(param);
         model.addAttribute("cars", numberOfCars);
         return "cars";
     }
